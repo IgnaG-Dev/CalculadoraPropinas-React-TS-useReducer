e4fdf08a35f215981086910704a2af04efc4d733
ignacioG-dev <ignaciogauto30@gmail.com>
2024-05-16 10:53:24 -0300

    Primer commit

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,53 @@
+import { useMemo } from "react";
+import { OrderItem } from "../types";
+import { formatCurrency } from "../helpers";
+import { OrderAction } from "../reducers/order-reducer";
+import { Dispatch } from "react";
+
+type OrderTotalsProps = {
+  order: OrderItem[];
+  tip: number;
+  dispatch: Dispatch<OrderAction>;
+};
+
+export default function OrderTotals({
+  order,
+  tip,
+  dispatch,
+}: OrderTotalsProps) {
+  const subTotalAmount = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+
+  const tipAmount = useMemo(() => subTotalAmount * tip, [tip, order]);
+
+  const totalAmount = useMemo(() => subTotalAmount + tipAmount, [tip, order]);
+
+  return (
+    <>
+      <div className="space-y-3">
+        <h2 className="font-black text-2xl">Totales y Propina:</h2>
+        <p>
+          Subtotal a Pagar: {""}
+          <span className="font-bold">{formatCurrency(subTotalAmount)}</span>
+        </p>
+        <p>
+          Propina: {""}
+          <span className="font-bold">{formatCurrency(tipAmount)}</span>
+        </p>
+        <p>
+          Total a Pagar: {""}
+          <span className="font-bold">{formatCurrency(totalAmount)}</span>
+        </p>
+      </div>
+      <button
+        className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
+        disabled={totalAmount === 0}
+        onClick={() => dispatch({ type: "place-order" })}
+      >
+        Guardar Orden
+      </button>
+    </>
+  );
+}
